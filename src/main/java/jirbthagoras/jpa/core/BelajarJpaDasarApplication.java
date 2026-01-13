1fa05cdcc5010add7969fb105573d665408aa83a
jirbthagoras <hansjabriel@gmail.com>
2026-01-13 23:26:39 +0700

    initialization

---
+++ src/main/java/jirbthagoras/jpa/core/BelajarJpaDasarApplication.java
@@ -0,0 +1,13 @@
+package jirbthagoras.jpa.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarJpaDasarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarJpaDasarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jirbthagoras/jpa/core/BelajarJpaDasarApplicationTests.java
@@ -0,0 +1,13 @@
+package jirbthagoras.jpa.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarJpaDasarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
